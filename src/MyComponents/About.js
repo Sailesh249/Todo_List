45509a3df3821bcab3f943a034240913b91d2e80
Sailesh <saileshdhayepule249@gmail.com>
2023-08-07 15:47:32 +0530

    todo app

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        This is an about component
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim laboriosam, perferendis voluptates quisquam dolores eos illum incidunt deleniti eligendi animi ad dolorem, beatae facere. Autem id illum aut non debitis nostrum, placeat doloribus quisquam!</p>
+    </div>
+  )
+}
